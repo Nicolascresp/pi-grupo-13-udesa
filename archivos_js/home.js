11ac3aa0fc9348dcdbe1b0b561ef91d54f302537
Nicolascresp <ncrespo@udesa.edu.ar>
2023-11-10 14:37:57 -0300

    Agregue el fetch del home

--- archivos_js/home.js
+++ archivos_js/home.js
@@ -1,19 +1,31 @@
-let peliculas = document.querySelector(".grupo1")
-
-console.log(peliculas)
-
-let peliculas_menor = document.querySelector(".card")
+let peliculas_menor = document.querySelector(".cajahija")
 
 let acaVaLaAPIKey = "0e2ea5762304016279ec676c08bd2b6d";
 
-fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=0e2ea5762304016279ec676c08bd2b6d")
+fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${acaVaLaAPIKey}`)
 .then(function(response){
     return response.json()
 })
 .then(function(data){
-    console.log(data)
+    console.log(data.results)
+    let arrayPeliculas = data.results
+    let peliculas = ""
+
+    for (let i = 0; i <= 5 ; i++) {
+        peliculas +=` <article class="articulo_pelicula">
+        <a href="./detalleP.html">
+        <img src="https://image.tmdb.org/t/p/w500/${arrayPeliculas[i].poster_path}"
+        class="img-p">
+        </a>
+        <h3 class="elem1">${arrayPeliculas[i].title}</h3>
+
+        <p class="info">Fecha de estreno: ${arrayPeliculas[i].release_date}</p>
+        </article> `;
+        
+    }
+    peliculas_menor.innerHtml += peliculas
     return data
 })
 .catch(function(error){
     console.log(error) 
-});
+})
