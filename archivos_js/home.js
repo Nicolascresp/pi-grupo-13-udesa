e4d0842eb3154fa4ee93cd6804211e8087f49ba2
VicenteBosch <vbosch@udesa.edu.ar>
2023-11-14 09:04:44 -0300

    modificaciones en el html y en el js de home

--- archivos_js/home.js
+++ archivos_js/home.js
@@ -1,8 +1,19 @@
-const peliculas_menor = document.querySelector(".grupo1")
+const apyKey = "0e2ea5762304016279ec676c08bd2b6d"
 
-let acaVaLaAPIKey = "0e2ea5762304016279ec676c08bd2b6d";
+const peliculasPopulares = `https://api.themoviedb.org/3/movie/popular?api_key=${apyKey}`
+const seriesPopulares = `https://api.themoviedb.org/3/tv/popular?api_key=${apyKey}`
+const peliculasMasValoradas = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apyKey}`
 
-fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${acaVaLaAPIKey}`)
+const buscador = `https://api.themoviedb.org/3/search/movie?api_key=${apyKey}&query=${busqueda}`
+
+
+let peliculas = document.querySelector(".hijo1") 
+
+let peliculas_menor = document.querySelector(".cajahija")
+
+
+
+fetch(peliculasPopulares)
 .then(function(response){
     return response.json()
 })
@@ -12,20 +23,18 @@ fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${acaVaLaAPIKey}`)
     let peliculas = ""
 
     for (let i = 0; i <= 5 ; i++) {
-        peliculas +=` <article class="articulopelicula">
+        peliculas +=` <article class="card">
         <a href="./detallePeliculas.html">
-        <img src="https://image.tmdb.org/t/p/w500/${arrayPeliculas[i].poster_path}"
-        class="img-p">
+        <img src="https://image.tmdb.org/t/p/w92/bvYjhsbxOBwpm8xLE5BhdA3a8CZ.jpg${arrayPeliculas[i].poster_path}"
+        class="imagenes">
         </a>
         <h3 class="elem1">${arrayPeliculas[i].title}</h3>
 
-        <p class="info">Fecha de estreno: ${arrayPeliculas[i].release_date}</p>
-
+        <p class="texto">Fecha de estreno: ${arrayPeliculas[i].release_date}</p>
         </article> `;
         
-        
     }
-    peliculas_menor.innerHtml += peliculas
+    peliculas.innerHtml += peliculas
     return data
 })
 .catch(function(error){
